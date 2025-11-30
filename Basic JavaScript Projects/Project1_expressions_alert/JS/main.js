e799f73f3434ce16c4d4731cad5d50f135f6b140
YassyAA-423 <yaaguilera42@gmail.com>
2025-11-29 19:15:50 -0700

    Basic JavaScript Projects Submission Assignment
    This is my submission for the Basic JavaScript Projects for the Tech Academy.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,26 @@
+//This is the pop-up/alert challenge
+// (Another example of alerts): alert('Hello Worlds!'); 
+var B = "Pop-up example";
+window.alert(B);
+
+//Concatenated string challenge example
+var Sent1 = "This is the beginning of the string ";
+var Sent2 = "and this is the end of the string<br>";
+var C = Sent1 + Sent2;
+
+document.write(C);
+
+//Multiple Variables Assignment
+var Pets = "The dogs", Dog1 = "Max<br>", Dog2 = "Tyke<br>", Dog3 = "Nina<br>";
+document.write(Dog3);
+
+
+//expression Challenge
+document.write(28 * 3)
+
+//Function button
+function My_First_Function() {             //defining a function and naming it
+    var str = "Testing";    //Defining a variable and giving it
+                                            //a string value
+     document.getElementById("Button_Text").innerHTML = str; // putting the value of the variable into the HTML elmentFromPoint with the "Button_Text" id
+}
