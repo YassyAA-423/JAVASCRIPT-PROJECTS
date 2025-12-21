2f74ca60c331fda4951cc4375738a0d273cbeb86
YassyAA-423 <yaaguilera42@gmail.com>
2025-12-20 21:33:15 -0700

    Academy-Cinemas Bootstrap Submission
    This is my submission for Academy-Cinemas Bootstrap

---
+++ Academy-Cinemas/javascript/javascript.js
@@ -0,0 +1,38 @@
+//initialize popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+
+popoverTriggerList.forEach(function (element) {
+    var imgSrc = element.getAttribute('data-bs-img');
+    var content = "<img class='star-rating' src='" + imgSrc + "'>";
+    new bootstrap.Popover(element, {
+        content: content,
+        html: true,
+        trigger: 'hover'
+    })
+});
+
+//initialize toast
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEL) {
+    return new bootstrap.Toast(toastEL)
+})
+
+//function to display toast with selected options
+function displaySelectedMovieOptions() {
+    var movie = document.getElementById('movieSelect').options[document.getElementById('movieSelect').selectedIndex].text;
+    var time = document.getElementById('timeSelect').options[document.getElementById('timeSelect').selectedIndex].text;
+    var quantity = document.getElementById('quantity').value;
+
+    var message = "Purchase confirmed for: " + movie + "\nTime: " + time + "\nTickets: " + quantity;
+
+    //Display Toast
+    var toastBody = document.getElementById('toastBody');
+    toastBody.textContent = message;
+    var toast = new bootstrap.Toast(document.getElementById('toastDisplay'));
+    toast.show()
+}
+
+function buyTickets(){
+    displaySelectedMovieOptions();
+}
+
