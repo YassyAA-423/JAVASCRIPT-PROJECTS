59d711d9419d65fe8f2c2c585f14603f2d9f48e7
YassyAA-423 <yaaguilera42@gmail.com>
2025-12-20 22:24:50 -0700

    Bootstrap with jQuery Submission
    This is my submission for Bootstrap with jQuery Submission. It includes:
    - A Shrink and scroll effect on the navbar
    -The mobile menu automatically closes when clicked

--- Academy-Cinemas/javascript/javascript.js
+++ Academy-Cinemas/javascript/javascript.js
@@ -36,3 +36,38 @@ function buyTickets(){
     displaySelectedMovieOptions();
 }
 
+//jQuery
+
+//Shrinks header size when the document is scrolled down by 80 px
+$(document).on("scroll", function(){
+    //When the webpage is scrolled down by 50px this if 
+    //statement will trigger
+    if($(document).scrollTop() > 50) {
+        //One the 50px requirment has been met add the nav-shrink 
+        //class selector to the same HTML element that has the
+        //nav-class
+        $("nav").addClass("nav-shrink");
+        //adjust the position of the mobile drop menu to accommodate
+        //the new height decrease
+        $("div.navbar-collapse").css("margin-top", "-6px");
+    } else {
+        //if the webpage has not been scrolled down or is back at the 
+        //top of the nav-shrink class selector is removed from the HTML
+        //element with the nav class selector
+        $("nav").removeClass("nav-shrink");
+        //The margin for the drop down menu is now returned to it's
+        //original amount
+        $("div.navbar-collapse").css("margin-top", "14px");
+    }
+});
+
+//close mobile menu when a nav link is clicked
+$(document).ready(function(){
+    //On click when and element contains just the nav-link class and not 
+    //the dropdown-toggle and then also close when an element with the class 
+    //.dropdown-item (each movie link) has been clicked
+    $(".navbar-nav").on('click', 'nav-link:not(".dropdown-toggle"), .dropdown-item', function(){
+        //collapse the navbar when a link or dropdown item is clicked
+        $(".navbar-collapse").collapse('hide');
+    });
+})
